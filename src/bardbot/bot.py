0ddf4cfe1d7315f1ab3a98e57d71e540ee750124
h587731 <mail.eskil@gmail.com>
2020-06-02 22:01:51 +0200

    Reorganize bot imports and remove legacy code

--- src/bardbot/bot.py
+++ src/bardbot/bot.py
@@ -8,21 +8,17 @@ import discord
 from discord import opus
 from discord.ext import commands, tasks
 from discord.utils import get
-import ffmpeg
-from pydub import AudioSegment as AS, effects
+from pydub import AudioSegment as dubas
 
 from .scene import Scene
 
 load_dotenv()
 BOT_TOKEN = os.getenv('TOKEN')
 GUILD_ID = os.getenv('GUILD_ID')
-
-
-
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+bot = commands.Bot(command_prefix='!')
 
 
-# noinspection PyDefaultArgument
 def load_opus_lib(opus_libs=OPUS_LIBS):
     if opus.is_loaded(): return True
     for opus_lib in opus_libs:
@@ -31,12 +27,11 @@ def load_opus_lib(opus_libs=OPUS_LIBS):
             return
         raise RuntimeError(f"Could not load an opus lib ({opus_lib}). Tried {', '.join(opus_libs)}")
 
-
 def ptype(obj):
     print(type(obj))
 
 
-bot = commands.Bot(command_prefix='!')
+
 
 
 @bot.event
@@ -64,25 +59,11 @@ async def test4(ctx, mp3):
     else:
         await voice.move_to(channel)
 
-    input4 = AS.from_mp3(mp3).set_frame_rate(48000)
+    input4 = dubas.from_mp3(mp3).set_frame_rate(48000)
     export = input4.export(format='opus', codec='libopus')
     voice.stop()
     voice.play(discord.FFmpegOpusAudio(export.raw, pipe=True, codec='libopus'))
 
-
-@bot.command()
-async def test3(ctx, mp3):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-    if not voice or not voice.is_connected():
-        voice = await channel.connect()
-        await voice.move_to(channel)
-    else:
-        await voice.move_to(channel)
-    print('test')
-    voice.stop()
-    voice.play(discord.PCMAudio(Gen_Wrapper(mp3)))
-    print('sgsgs')
 
 
 @bot.command()
@@ -96,13 +77,7 @@ async def test2(ctx, mp3):
     else:
         await voice.move_to(channel)
 
-    # input1 = ffmpeg.input()
-    # input1 = ffmpeg.input()
-    # input1 = ffmpeg.input()
-    # input1 = ffmpeg.input()
-    # input1 = ffmpeg.input()
-
-    out = AS.from_mp3(mp3).set_frame_rate(48000).set_sample_width(2)
+    out = dubas.from_mp3(mp3).set_frame_rate(48000).set_sample_width(2)
     out = io.BytesIO(out.raw_data)
     ptype(out)
 
@@ -110,48 +85,13 @@ async def test2(ctx, mp3):
     voice.play(discord.PCMAudio(out))
 
 
-@bot.command()
-async def test1(ctx, mp3):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-    if not voice or not voice.is_connected():
-        voice = await channel.connect()
-        await voice.move_to(channel)
-
-    else:
-        await voice.move_to(channel)
-
-    out, _ = (ffmpeg
-              .input(mp3)
-              .output('-', format='opus', acodec='libopus', ab='12800', ar='48k')
-              .overwrite_output()
-              .run(capture_stdout=True)
-              )
-    print(type(out))
-    out = io.BytesIO(out)
-    ptype(out)
-
-    voice.stop()
-    voice.play(discord.PCMAudio(out), after=my_after)
-
-
 def my_after(error):
-    # voice = get(bot.voice_clients)
-    # coro = voice.play(discord.PCMAudio(next(gen)), after=my_after)
-    #
-    # fut = asyncio.run_coroutine_threadsafe(coro, bot.loop)
-    # ptype(fut)
-    # try:
-    #     fut.result()
-    # except:
-    #     # an error happened sending the message
-    #     pass
-    print('player play done')
+   print('Player ended, empty stream')
 
 
 class Gen_Wrapper(discord.AudioSource):
     def __init__(self, mp3):
-        self.seg = AS.from_mp3(mp3).set_frame_rate(48000)[::20]
+        self.seg = dubas.from_mp3(mp3).set_frame_rate(48000)[::20]
 
     def read(self, _):
         return next(self.seg).raw_data
@@ -188,17 +128,17 @@ async def test(ctx, url):
 
     # slow_count.start(mix, buf)
 
-    print('playing')
+    print('Discord player playing from stream')
     voice.play(discord.PCMAudio(Mix_Wrapper(buf)), after=my_after)
 
 
 def get_set(url):
+    print("Setting up scene and buffer.")
     scene = Scene(url)
     buff = bRingBuf(3840000)
     while buff.len < 384000:
         buff.enqueue(next(scene.gen).raw_data)
-
-    print('ready set go')
+    print("Buffer ready.")
     return scene, buff
 
 
@@ -232,53 +172,7 @@ class StreamRW(io.BufferedRandom):
 
 
 
-@bot.command()
-async def play(ctx, url):
-    pass
-
-    file = ambient.get_mix_from_url(url)
-
-    channel = ctx.message.author.voice.channel
-    print('channel', channel)
-    print(type(channel))
-    if not channel:
-        await ctx.send("You are not connected to a voice channel")
-        return
-
-    # voice er none her feil
-
-    source = discord.FFmpegOpusAudio(file)
-
-    voice = get(bot.voice_clients, guild=ctx.guild)
-    if not voice or not voice.is_connected():
-        voice = await channel.connect()
-        await voice.move_to(channel)
-
-    else:
-        await voice.move_to(channel)
-
-    print(voice, 'voivoiv')
-
-    voice.stop()
-    voice.play(source)
-    print('kakaka')
-
-    # await songs.put(player)
-
-
-# print('source ', type(source))
-# if voice.is_playing():
-#     voice.stop()
-# voice.play(source)
-# # , after=lambda _: bot.loop.call_soon_threadsafe(play_next(voice))
-# channel = ctx.author.voice.channel
-# vc = await channel.connect()
-# vc.play(discord.FFmpegPCMAudio('tette.mp3'), after=lambda e: print('done', e))
-
-# for channel in mix.channels.values():
-
-# await ctx.send(channel.__dict__)
-
 # load_opus_lib()
+
 def main():
     bot.run(BOT_TOKEN)
